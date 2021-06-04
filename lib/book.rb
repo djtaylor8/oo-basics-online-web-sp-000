8a800da1a960eeb1c30076ff512238da2a9b8e4a
DJ Taylor <dtaylor88@gmail.com>
2021-06-04 03:11:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,6 @@
 class Book
   
+<<<<<<< HEAD
   attr_accessor :author, :page_count, :genre 
   attr_reader :title 
   
@@ -10,6 +11,40 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+=======
+  def initialize(title)
+    @title = title
+  end  
+  
+  def title
+    @title
+  end 
+  
+  def author=(author_name)
+    @author = author_name
+  end 
+  
+  def author 
+    @author 
+  end
+  
+  def page_count=(num)
+    @page_count = num 
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre 
+  end
+  
+  def genre
+    genre
+  end 
+  
+>>>>>>> 4cd4bb54c0d41fe22d253b6197a376e333f9622a
 end
 
 
